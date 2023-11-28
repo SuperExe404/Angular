29d201f048238bbc85b5bf097eff33f30eda7c95
SuperExe404 <diegomon0440@gmail.com>
2023-11-28 10:01:11 -0600

    02 Create Home COmponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import {Component} from '@angular/core';
-
+import {HomeComponent} from './home/home.component';
+import {RouterLink, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent, RouterLink, RouterOutlet],
   template: `
-    <h1>Hello world!</h1>
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
   `,
   styleUrls: ['./app.component.css'],
 })
